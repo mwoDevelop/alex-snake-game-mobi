ee2f4b74738a45a926a3b01746107b41d4949a8c
mwodevelop <mwodevelop@gmail.com>
2024-12-12 20:53:34 +0000

    Test

--- lib/game/snake_game.dart
+++ lib/game/snake_game.dart
@@ -71,6 +71,7 @@ class _SnakeGameState extends State<SnakeGame> {
   }
 
   void _handleSwipe(Direction direction) {
+    print('Swipe direction: $direction');
     if (!isGameOver) {
       if (direction == Direction.up && snake.direction != Direction.down) {
         nextDirection = Direction.up;
@@ -102,6 +103,7 @@ class _SnakeGameState extends State<SnakeGame> {
         }
       },
       onVerticalDragUpdate: (details) {
+        print('Vertical drag: ${details.delta.dy}');
         if (details.delta.dy < 0) {
           _handleSwipe(Direction.up);
         } else {
@@ -109,6 +111,7 @@ class _SnakeGameState extends State<SnakeGame> {
         }
       },
       onHorizontalDragUpdate: (details) {
+        print('Horizontal drag: ${details.delta.dx}');
         if (details.delta.dx < 0) {
           _handleSwipe(Direction.left);
         } else {
